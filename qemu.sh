9e43b98bb9807478154c19062ce283b65ef90367
Marc Becker <becm@gmx.de>
2017-09-14 18:29:57 +0200

    use `getopts` in `qemu` wrapper script
    add option to force raw image format
    make shared folder non-positional argument

--- qemu.sh
+++ qemu.sh
@@ -1,26 +1,41 @@
 #!/bin/sh
 # live-tools/qemu.sh: very simple qemu wrapper
 
-# select qemu interface
-unset mode
-[ "${1}" = "-S" ] && { mode='-nographic'; shift; }
+# initialize used variables
+image="discard=unmap,detect-zeroes=unmap,if=virtio"
+unset args
 
-# disk image setup
+# process argument flags
+while getopts 'SRs:' opt; do
+    case $opt in
+      # select qemu interface
+      S) args="${args} -nographic"
+         ;;
+      # select image format
+      R) image="format=raw,${image}"
+         ;;
+      # add shared folder
+      s) args="${args} -fsdev local,security_model=none,id=qshare,path=${OPTARG}"
+         args="${args} -device virtio-9p-pci,fsdev=qshare,mount_tag=share"
+         ;;
+      # bad command argument
+      \?)
+         printf '%s: %s\n' 'error' 'bad argument' 1>&2;
+         exit 1
+         ;;
+    esac
+done
+
+# consume flags
+shift $((OPTIND-1))
+
+# disk image check
 [ -n "${1}" -a -r "${1}" ] || {
     printf "%s: %s\n" "${0}" 'need disk image' 1>&2
     exit 1
 }
-image="file=${1},discard=unmap,detect-zeroes=unmap,if=virtio"
+image="${image},file=${1}"
 shift
 
-# add shared folder
-unset share
-[ -n "${1}" ] && {
-    [ ! "${1}" = "--" ] && {
-        share="-fsdev local,id=qshare,path=${1},security_model=none -device virtio-9p-pci,fsdev=qshare,mount_tag=share"
-    }
-    shift
-}
-
 # start VM instance
-exec qemu-system-x86_64 -enable-kvm -boot order=c -m 1G -drive "${image}" $share $mode "$@"
+exec qemu-system-x86_64 -enable-kvm -boot order=c -m 1G -drive "${image}" ${args} "$@"
